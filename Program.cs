73e6187989f4a3f1f46ad7a1f081e5e2327c6e6b
kundan <goswamikundan123@gmail.com>
2023-02-09 11:32:11 +0530

    Adding file to calculate Daily Employee Wage

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            CheckEmployee.EmployeeCheck();
+            //CheckEmployee.EmployeeCheck();
+            DailyEmplyeeWage.Employeewage();
         }
     }
 }
