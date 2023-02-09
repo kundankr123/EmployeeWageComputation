6d758d5366be0e58e54b1505c0a177df660fe032
kundan <goswamikundan123@gmail.com>
2023-02-09 11:03:30 +0530

    Adding CheckEmployee file to check he is present or absent

---
+++ CheckEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagecomputation
+{
+    public class CheckEmployee
+    {
+        public static void EmployeeCheck()
+        {
+            int Is_full_time = 1;
+            Random random = new Random ();
+            int empCheck = random.Next(2);
+
+            if (empCheck == Is_full_time) 
+            {
+                Console.WriteLine("Employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+        }
+    }
+}
