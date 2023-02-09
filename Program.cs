758967b90aabe941e10058fe23e8afee4aa30f9f
kundan <goswamikundan123@gmail.com>
2023-02-09 10:37:02 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWagecomputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
